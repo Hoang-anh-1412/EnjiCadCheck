bb1fde8c302ae59223451362ea2c301562f0a454
Hoang-anh-1412 <anh_nh@tamada.vn>
2026-07-13 10:33:35 +0700

    commit

---
+++ Class2.cs
@@ -0,0 +1,28 @@
+﻿using Gssoft.Gscad.EditorInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace check
+{
+    public class Class2
+    {
+        public static void ReportAsm(Editor ed, string name)
+        {
+            var loaded = Array.Find(
+                AppDomain.CurrentDomain.GetAssemblies(),
+                a => string.Equals(a.GetName().Name, name, StringComparison.OrdinalIgnoreCase));
+
+            if (loaded == null)
+            {
+                ed.WriteMessage("\n{0}: (not loaded)", name);
+                return;
+            }
+
+            ed.WriteMessage("\n{0}: {1}", name, loaded.Location);
+            ed.WriteMessage("\n         v{0}", loaded.GetName().Version);
+        }
+    }
+}
